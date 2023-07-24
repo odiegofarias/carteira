e394a9c99b911b36f379a34b0be1f7ee6c442e10
Diego Farias <diego.farias.fernandes@gmail.com>
2023-07-24 18:56:53 -0300

    Update views .py

--- ativo/views.py
+++ ativo/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    # ativos = Ativo.objects.values('nome').annotate(total=Sum('valor_aplicado')).order_by('nome')
+
+    
+    return render(request, 'index.html', {'ativos': ativos})
